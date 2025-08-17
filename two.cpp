162c24c8af494ab963f75c3d9dd57fd57b856a34
hoyeong Yoo <crystal23733@gmail.com>
2025-08-18 03:51:17 +0900

    업캐스팅
    FlyAnimal의 z_pos는 부모가 알지 못하기 때문에 없어져버림
    안전하게 업캐스팅됨

--- two.cpp
+++ two.cpp
@@ -4,20 +4,30 @@ using namespace std;
 struct Animal {
     float x_pos = 1;
     float y_pos = 2;
+    virtual ~Animal() {};
 };
 
 struct FlyAnimal: public Animal {
     float z_pos = 3;
 };
 
-void printAnimals(Animal *a, int n) {
+void printAnimals(Animal **a, int n) {
     for (int i = 0; i < n; i++) {
-        cout << "(" << a[i].x_pos << ", " << a[i].y_pos << ")" << endl;
+        cout << "(" << a[i]->x_pos << ", " << a[i]->y_pos << ")" << endl;
     }
 };
 
 int main() {
-    FlyAnimal *arr = new FlyAnimal[5];
-    printAnimals(arr, 5);
-    delete[] arr;
+    Animal **a = new Animal*[5];
+    for (int i = 0; i < 5; i++) {
+        a[i] = new FlyAnimal;
+    }
+
+    printAnimals(a, 5);
+
+    for (int i = 0; i < 5; i++) {
+        delete a[i];
+    }
+
+    delete[] a;
 }
